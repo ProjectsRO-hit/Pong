0c69c93355706509278377f0318f8e0f03fee3ee
ProjectsRO-hit <official.rohitsingh22@gmail.com>
2025-01-03 21:26:51 +0530

    Initial commit

---
+++ display.py
@@ -0,0 +1,32 @@
+from turtle import Screen, Turtle
+
+class Display:
+    def __init__(self):
+        self.screen = None
+        self.divider = Turtle()
+
+    def make_screen(self):
+        self.screen = Screen()
+        self.screen.title("Pong")
+        self.screen.bgcolor("black")
+        self.screen.setup(width=800, height=600)
+        self.screen.tracer(0)
+
+    def exit_screen(self):
+        self.screen.exitonclick()
+
+    def draw_divider(self):
+        self.divider.hideturtle()
+        self.divider.penup()
+        self.divider.setposition(0,300 )
+        self.divider.setheading(270)
+        self.divider.pendown()
+        self.divider.color("white")
+        self.divider.pensize(5)
+        for i in range(30):
+            self.divider.forward(10)
+            self.divider.pencolor("black")
+            self.divider.forward(10)
+            self.divider.pencolor("white")
+        self.divider.hideturtle()
+        self.make_screen()
---
+++ game.py
@@ -0,0 +1,10 @@
+from display import Display
+
+#import screen
+screen_build = Display()
+screen = screen_build.make_screen()
+screen_build.draw_divider()
+
+
+
+screen_build.exit_screen()
