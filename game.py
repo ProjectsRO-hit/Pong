6ad3c1b96b5916695e978e3ee977926d21dc8315
ProjectsRO-hit <official.rohitsingh22@gmail.com>
2025-01-03 23:34:41 +0530

    final

--- game.py
+++ game.py
@@ -1,5 +1,9 @@
+import ball
 from display import Display
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 #import screen
 screen_build = Display()
@@ -9,6 +13,10 @@ screen_build.draw_divider()
 #make paddle
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
+#make ball
+pong_ball = Ball()
+#make scoreboard
+scoreboard = Scoreboard()
 
 
 #move paddles
@@ -18,11 +26,31 @@ screen_build.screen.onkeypress(r_paddle.move_down, "Down")
 screen_build.screen.onkeypress(l_paddle.move_up, "w")
 screen_build.screen.onkeypress(l_paddle.move_down, "s")
 
-
 game_on = True
 
 while game_on:
+    time.sleep(pong_ball.move_speed)
     screen_build.screen.update()
+    pong_ball.move_ball()
+
+    #Detect collision with wall
+    if pong_ball.ycor() > 280 or pong_ball.ycor() < -280:
+        #need to bounce
+        pong_ball.bounce_y()
+
+    # Detect collision with r_paddle
+    if pong_ball.distance(r_paddle) < 50 and pong_ball.xcor() > 320 or pong_ball.distance(l_paddle) < 50 and pong_ball.xcor() < -320:
+        pong_ball.bounce_x()
+
+    #Detect right paddle misses
+    if pong_ball.xcor() > 380:
+        pong_ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect left paddle misses
+    if pong_ball.xcor() < -380:
+        pong_ball.reset_position()
+        scoreboard.r_point()
 
 
 #delete screen
